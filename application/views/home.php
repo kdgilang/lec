ec1cb2de7c9ea471929727b2318b29d944a972bb
tryagus <itisnoont@gmail.com>
2017-10-14 11:59:26 +0800

    update

--- application/views/home.php
+++ application/views/home.php
@@ -172,7 +172,30 @@
           <div class="col-xs-12 text-center">
             <h2 class="section-heading">Kontak</h2>
             <hr class="primary">
-            <!-- <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p> -->
+              <div class="call-to-action">
+                <div class="container text-center">
+                    <p>
+                    1. LEC BALI adalah tempat belajar bahasa Inggris dengan metode private home (Visit Student)</p>
+                    <p>
+                      2. Peserta didik baru dikenakan <b>Registrasi Fee Rp. 100.000</b> + Uang Prive 50 %
+                    </p>
+                    <p>
+                      3. Setiap Level selama <b><i>total 15 jam</i></b> dengan jadwal (3 x seminggu) atau (2 x seminggu) dan <b><i>harus selesai dalam 2 Bulan</i></b>
+                    </p>
+                    <p>
+                      4. Biaya private di bayar full setelah 5x pertemuan
+                    </p>
+                    <p>
+                      5. Perubahan jadwal diberitahukan 5 jam sebelum jadwal pertemuan dan apabila di cancel secara tiba-tiba, maka itu dianggap hangus dari total pertemuan.
+                    </p>
+                    <p>
+                      6. Uang yang sudah diberikan tidak bisa dikembalikan, tanpa ada perjanjian sebelumnya.
+                    </p>
+                    <p>
+                      7. Learning smart, not Learning Hard
+                    </p>
+                </div>
+              </div>
           </div>
         </div>
         <div class="row">
