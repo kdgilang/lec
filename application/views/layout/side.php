90ea07ce9581e11a4ec758811eda81e0881bca8e
kdgilang <gilang.putra@ogilvy.com.au>
2017-10-14 15:11:14 +0800

    UPDATE

--- application/views/layout/side.php
+++ application/views/layout/side.php
@@ -1,5 +1,4 @@
 <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-
 <div class="col-xs-12 col-sm-3 col-md-2 clear-md fixed-top">
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav side-nav">
@@ -14,24 +13,24 @@
                 <a class="nav-link" href="<?= base_url('kelas'); ?>"><i class="fa fa-calendar icon-menu" aria-hidden="true"></i> Kelas Kursus</a>
             </li>  
             <li>
-                <a class="nav-link" href="<?= base_url('operator'); ?>"><i class="glyphicon glyphicon-user icon-menu" aria-hidden="true"></i>Operator</a>
+                <a class="nav-link" href="<?= base_url('operator'); ?>"><i class="icon-menu fa fa-users" aria-hidden="true"></i> Operator</a>
             </li>
             <li>
-                <a class="nav-link" href="<?= base_url('pengajar'); ?>"><span class="glyphicon glyphicon-user icon-menu" aria-hidden="true"></span> Pengajar</a>
+                <a class="nav-link" href="<?= base_url('pengajar'); ?>"><span class="icon-menu fa fa-users" aria-hidden="true"></span> Pengajar</a>
             </li>   
             <li>
-                <a class="nav-link" href="<?= base_url('siswa'); ?>"><span class="glyphicon glyphicon-user icon-menu" aria-hidden="true"></span> Siswa</a>
+                <a class="nav-link" href="<?= base_url('siswa'); ?>"><span class="icon-menu fa fa-users" aria-hidden="true"></span> Siswa</a>
             </li>  
             <li>
-                <a class="nav-link" href="<?= base_url('siswa'); ?>"><span class="icon-menu fa fa-info-circle" aria-hidden="true"></span>Pengumuman</a>
+                <a class="nav-link" href="<?= base_url('siswa'); ?>"><span class="icon-menu fa fa-info-circle" aria-hidden="true"></span> Pengumuman</a>
             </li> 
             <li>
-                <a class="nav-link" href="<?= base_url(); ?>"><span class="icon-menu fa fa-certificate" aria-hidden="true"></span>Sertifikat</a>
+                <a class="nav-link" href="<?= base_url(); ?>"><span class="icon-menu fa fa-certificate" aria-hidden="true"></span> Sertifikat</a>
             </li> 
             <?php }?>  
             <?php if ($this->session->level == 4) {?>
             <li>
-                <a class="nav-link" href="<?= base_url('siswa'); ?>"><span class="icon-menu fa fa-info-circle" aria-hidden="true"></span>Pengumuman</a>
+                <a class="nav-link" href="<?= base_url('siswa'); ?>"><span class="icon-menu fa fa-info-circle" aria-hidden="true"></span> Pengumuman</a>
             </li> 
             <li>
                 <a class="nav-link" href="<?= base_url(); ?>"><span class="icon-menu fa fa-calendar" aria-hidden="true"></span> Jadwal Kursus</a>
